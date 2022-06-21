03c001b9ff16055600555f266632645b6f060d56
Kami-Juan <juan1910@live.com.mx>
2022-06-20 23:04:08 -0500

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { useState } from 'react';
+import Button from '@mui/material/Button';
+
+function App() {
+  return (
+    <div className="App">
+      <Button variant="contained">Hola mundo</Button>
+    </div>
+  );
+}
+
+export default App;
